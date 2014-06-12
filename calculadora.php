1b474979f0ac5e38b7514a98a545cc1c8c2d2092
freedfelipe <freedfelipe@gmail.com>
2014-06-11 21:25:33 -0300

    Snnifer em codigo com ajuste de tabulacoes e comentarios

--- calculadora.php
+++ calculadora.php
@@ -1,109 +1,157 @@
-<?
-ini_set('display_errors',1);
+<?php
+/**
+* Class para realizar calculos basicos de uma calculadora
+*
+* PHP version 5
+*
+* @category     PHP
+* @package      Calculadora
+* @author       Thiago Ramos <thiagoevangelista.contato@gmail.com>
+* @copyright    1997-2005 The PHP Group
+* @license      http://www.php.net/license/3_01.txt  PHP License 3.01
+* @version      SVN: 1.0.1
+* @since        File available since Release 1.0.1
+* @deprecated   File deprecated in Release 2.0.0
+*/
 
-class Calculadora{
+namespace calculadora;
+
+class Calculadora
+{
+    // varivaveis globais para o calculo
+    protected $number1 = 0;
+    protected $number2 = 0;
+    protected $operator = false;
+    protected $result = false;
     
-    protected $number1=0,$number2=0,$operator=false,$result=false;
-
-    public function setValor1($num){
-        
-        if(is_numeric($num) && $num>0){
-              $this->number1=$num;
-        }else{
-             throw new RuntimeException('Valor '.$num.' é inválido para calculo');
+    /**
+    * Seta o primeiro valor do calculo
+    * @param $num
+    * @author Thiago Ramos
+    **/
+    public function setValor1($num)
+    {
+        if (is_numeric($num) && $num > 0) {
+            $this->number1 = $num;
+        } else {
+            throw new RuntimeException('Valor '.$num.' é inválido para calculo');
         }
-             
     }
-
- 
- 
-    public function setValor2($num){
-        
-        if(is_numeric($num) && $num>0){
-              $this->number2=$num;
-        }else{
-             throw new RuntimeException('Valor '.$num.' é inválido para calculo');
+    
+    /**
+    * Seta o segundo valor do calculo
+    * @param $num
+    * @author Thiago Ramos
+    **/
+    public function setValor2($num)
+    {
+        if (is_numeric($num) && $num > 0) {
+            $this->number2 = $num;
+        } else {
+            throw new RuntimeException('Valor '.$num.' é inválido para calculo');
         }
-                 
     }
-
-        
-    public function getNumber1(){
+    
+    /**
+    * Pega o primeiro valor para o calculo
+    * @param $this->number1
+    * @author Thiago Ramos
+    * @return num
+    **/
+    public function getNumber1()
+    {
         return $this->number1;
     }
     
-    
-    
-    public function getNumber2(){
+    /**
+    * Pega o segundo valor para o calculo
+    * @param $this->number2
+    * @author Thiago Ramos
+    * @return num
+    **/
+    public function getNumber2()
+    {
         return $this->number2;
     }
     
-    
-    
- 
-    public function setOperator($operator){        
-          
-            switch($operator){
-              case '+':
-                  $this->operator='+';    
-              break;
-              case '-':
-                  $this->operator='-';    
-              break;
-              case '/':
-                  $this->operator='/';    
-              break;
-              case '*':
-                  $this->operator='*';    
-              break;
-              default:
-                 throw new RuntimeException('Operador '.$operator.' é inválido para calculo');
-              break;            
-            }
+    /**
+    * Seta o operador para o calculo
+    * @param $operator
+    * @author Thiago Ramos
+    **/
+    public function setOperator($operator)
+    {
+        switch ($operator) {
+            case '+':
+                $this->operator = '+';
+                break;
+            case '-':
+                $this->operator = '-';
+                break;
+            case '/':
+                $this->operator = '/';
+                break;
+            case '*':
+                $this->operator = '*';
+                break;
+            default:
+                throw new RuntimeException('Operador '.$operator.' é inválido para calculo');
+                break;
+        }
     }
     
-    
-    public function getOperator(){
-        if($this->operator!==false){
+    /**
+    * Seta o operador para o calculo
+    * @param $this->operator
+    * @author Thiago Ramos
+    * @return $this->operator
+    * @return false
+    **/
+    public function getOperator()
+    {
+        if ($this->operator !== false) {
             return $this->operator;
         }
         
         return false;
     }
     
-    public function setResult(){
-        
-        if($this->number1>0 && $this->number2>0 && $this->operator!==false){
+    /**
+    * Realiza o calculo
+    * @param $this->number1, $this->number2, $this->operator
+    * @author Thiago Ramos
+    **/
+    public function setResult()
+    {
+        if ($this->number1 > 0 && $this->number2 > 0 && $this->operator !== false) {
             
-            
-             switch($this->operator){
-              case '+':
-                  $this->result=$this->number1+$this->number2;
-              break;
-              case '-':
-                  $this->result=$this->number1-$this->number2;
-              break;
-              case '/':
-                  $this->result=$this->number1/$this->number2;
-              break;
-              case '*':
-                  $this->result=$this->number1*$this->number2;
-              break;
-              
+            switch($this->operator){
+                case '+':
+                    $this->result = $this->number1 + $this->number2;
+                    break;
+                case '-':
+                    $this->result = $this->number1 - $this->number2;
+                    break;
+                case '/':
+                    $this->result = $this->number1 / $this->number2;
+                    break;
+                case '*':
+                    $this->result = $this->number1 * $this->number2;
+                    break;
             }
-            
-            
-            
         }
     }
     
-    public function getResult(){
+    /**
+    * Retorna o valor do calculo
+    * @author Thiago Ramos
+    * @return $this->result
+    **/
+    public function getResult()
+    {
         return $this->result;
     }
-    
 }
 
-
-
-
-?>
+//End of file calculadora.php
+//Location: ~calculadora.php
